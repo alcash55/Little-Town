350a8b71a209b1f6d4078dfbf55d3b597a218f28
Sami <sami.niemi@live.fi>
2023-03-20 19:31:54 +0200

    Refactor flask app

---
+++ backend/flask_app/app/events/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+bp = Blueprint('events', __name__)
+
+
+from app.events import routes
--- backend/flask_app/app/events/routes.py
+++ backend/flask_app/app/events/routes.py
@@ -1,35 +1,35 @@
-from flask import Blueprint, jsonify, request
-
-from database import db
-from models.event import Event
-
-event_controller = Blueprint("event_controller", __name__)
+from app.events import bp
+from app.extensions import db
+from app.models.event import Event
+from flask import jsonify, render_template, request
 
 
-@event_controller.route("/events", methods=["GET"])
+@bp.route('/')
+def index():
+    return render_template('events/index.html')
+
+@bp.route("/events", methods=["GET"])
 def get_events():
     events = Event.query.all()
     return jsonify([event.to_dict() for event in events]), 200
 
 
-@event_controller.route("/events", methods=["POST"])
+@bp.route("/events", methods=["POST"])
 def create_event():
     data = request.get_json()
-    event = Event(
-        name=data["name"], description=data.get("description"), date=data.get("date")
-    )
+    event = Event(name=data["name"], date=data["date"], location=data["location"])
     db.session.add(event)
     db.session.commit()
     return jsonify(event.to_dict()), 201
 
 
-@event_controller.route("/events/<int:event_id>", methods=["GET"])
+@bp.route("/events/<int:event_id>", methods=["GET"])
 def get_event(event_id):
     event = Event.query.get_or_404(event_id)
     return jsonify(event.to_dict()), 200
 
 
-@event_controller.route("/events/<int:event_id>", methods=["PUT"])
+@bp.route("/events/<int:event_id>", methods=["PUT"])
 def update_event(event_id):
     event = Event.query.get_or_404(event_id)
     data = request.get_json()
@@ -40,7 +40,7 @@ def update_event(event_id):
     return jsonify(event.to_dict()), 200
 
 
-@event_controller.route("/events/<int:event_id>", methods=["DELETE"])
+@bp.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
     event = Event.query.get_or_404(event_id)
     db.session.delete(event)
