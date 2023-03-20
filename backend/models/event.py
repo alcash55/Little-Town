4cf209205599cde47a086efc74b6511a59aab672
Sami <sami.niemi@live.fi>
2023-03-20 17:14:16 +0200

    Add initial backend

---
+++ backend/models/event.py
@@ -0,0 +1,16 @@
+from database import db
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(1024), nullable=False)
+    date = db.Column(db.Date, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "date": str(self.date),
+        }
