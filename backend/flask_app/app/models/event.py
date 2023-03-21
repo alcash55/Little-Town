df899b05fd2931f04199602ce0060ffd41e24ce1
Sami <sami.niemi@live.fi>
2023-03-21 23:16:21 +0200

    Test migration

--- backend/flask_app/app/models/event.py
+++ backend/flask_app/app/models/event.py
@@ -2,15 +2,15 @@ from app.extensions import db
 
 
 class Event(db.Model):
+    __tablename__ = "events"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(1024), nullable=False)
-    date = db.Column(db.Date, nullable=True)
 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
             "description": self.description,
-            "date": str(self.date),
         }
