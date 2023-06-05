07db9315ba4d63bc905a70bf28eea5163715d34c
Alex <alex.e.cash28@gmail.com>
2023-06-05 17:12:35 -0400

    clean up app exports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/app/App";
+import { App } from "./components/app/App";
 import "./assets/styles/globalLayout.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
