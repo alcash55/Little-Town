93435998107ff3db8ee138b537579a18619c9727
Alex <alex.e.cash28@gmail.com>
2023-06-05 18:59:34 -0400

    center content

--- frontend/src/components/pages/Bingo/Bingo.tsx
+++ frontend/src/components/pages/Bingo/Bingo.tsx
@@ -1,7 +1,19 @@
 import { Box } from "@mui/material";
 
 const Bingo = () => {
-  return <Box sx={{ bgcolor: "#424242", height: "100%" }}>Bingo</Box>;
+  return (
+    <Box
+      sx={{
+        bgcolor: "#424242",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Bingo
+    </Box>
+  );
 };
 
 export default Bingo;
