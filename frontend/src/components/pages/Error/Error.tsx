598643c287c34889c3e6b05c2f3644891c6c987e
Alex <alex.e.cash28@gmail.com>
2023-06-05 18:57:27 -0400

    configure page height

--- frontend/src/components/pages/Error/Error.tsx
+++ frontend/src/components/pages/Error/Error.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 
 const Error = () => {
-  return <Box>404</Box>;
+  return <Box sx={{ bgcolor: "#424242", height: "100%" }}>404</Box>;
 };
 
 export default Error;
--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 
 const Home = () => {
-  return <Box>home</Box>;
+  return <Box sx={{ bgcolor: "#424242", height: "100%" }}>home</Box>;
 };
 
 export default Home;
