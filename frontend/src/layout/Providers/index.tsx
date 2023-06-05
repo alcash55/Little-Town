06ca107d370b424cb8129a6c31cea09c7a433834
Alex <alex.e.cash28@gmail.com>
2023-06-05 18:57:56 -0400

    add base url name

--- frontend/src/layout/Providers/index.tsx
+++ frontend/src/layout/Providers/index.tsx
@@ -6,7 +6,7 @@ import { SidebarProvider } from "../../contexts";
 
 export function Providers({ children }: PropsWithChildren<{}>) {
   return (
-    <Router>
+    <Router basename="/littletown">
       <SidebarProvider>
         <ThemeProvider>{children}</ThemeProvider>
       </SidebarProvider>
