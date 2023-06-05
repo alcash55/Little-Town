00c6cdecb7fb20e3e2e88b3e1bf0aa4da663f2eb
Alex <alex.e.cash28@gmail.com>
2023-06-05 18:57:27 -0400

    configure page height

--- frontend/src/components/pages/Bingo/Bingo.tsx
+++ frontend/src/components/pages/Bingo/Bingo.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 
 const Bingo = () => {
-  return <Box>Bingo</Box>;
+  return <Box sx={{ bgcolor: "#424242", height: "100%" }}>Bingo</Box>;
 };
 
 export default Bingo;
