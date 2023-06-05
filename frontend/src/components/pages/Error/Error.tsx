e9b7452362b4438556336d657f797b18fa32b430
Alex <alex.e.cash28@gmail.com>
2023-06-05 18:59:34 -0400

    center content

--- frontend/src/components/pages/Error/Error.tsx
+++ frontend/src/components/pages/Error/Error.tsx
@@ -1,7 +1,19 @@
 import { Box } from "@mui/material";
 
 const Error = () => {
-  return <Box sx={{ bgcolor: "#424242", height: "100%" }}>404</Box>;
+  return (
+    <Box
+      sx={{
+        bgcolor: "#424242",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      404
+    </Box>
+  );
 };
 
 export default Error;
--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,7 +1,19 @@
 import { Box } from "@mui/material";
 
 const Home = () => {
-  return <Box sx={{ bgcolor: "#424242", height: "100%" }}>home</Box>;
+  return (
+    <Box
+      sx={{
+        bgcolor: "#424242",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      home
+    </Box>
+  );
 };
 
 export default Home;
