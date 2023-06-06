fdf97b5b239eedd1e5e88049810b39256b7896d9
Alex <alex.e.cash28@gmail.com>
2023-06-06 14:51:48 -0400

    updated deploy script

--- frontend/src/layout/Providers/index.tsx
+++ frontend/src/layout/Providers/index.tsx
@@ -6,7 +6,7 @@ import { SidebarProvider } from "../../contexts";
 
 export function Providers({ children }: PropsWithChildren<{}>) {
   return (
-    <Router basename="/lt-event-app">
+    <Router basename="/littletown">
       <SidebarProvider>
         <ThemeProvider>{children}</ThemeProvider>
       </SidebarProvider>
