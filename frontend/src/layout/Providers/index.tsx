8dbae62ff2aab1b8c68b420a2242e8924992a492
Alex <alex.e.cash28@gmail.com>
2023-06-06 14:17:45 -0400

    updated basename

--- frontend/src/layout/Providers/index.tsx
+++ frontend/src/layout/Providers/index.tsx
@@ -6,7 +6,7 @@ import { SidebarProvider } from "../../contexts";
 
 export function Providers({ children }: PropsWithChildren<{}>) {
   return (
-    <Router basename="https://culinarist.github.io/lt-event-app/">
+    <Router basename="/lt-event-app">
       <SidebarProvider>
         <ThemeProvider>{children}</ThemeProvider>
       </SidebarProvider>
