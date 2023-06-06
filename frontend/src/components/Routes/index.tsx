88ea3fda9cfa9c42f770e8cbb83d43ac3e401a34
Alex <alex.e.cash28@gmail.com>
2023-06-06 15:25:41 -0400

    updated deploy script

--- frontend/src/components/Routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useRoutes } from "react-router-dom";
-import { routes } from "./routes";
-
-export const AllRoutes = () => {
-  const allRoutes = useRoutes(routes);
-
-  return allRoutes;
-};
