e30d1e3753c5b8d4a402e53f9dfa317fc04bc831
Alex <alex.e.cash28@gmail.com>
2023-06-08 00:37:37 -0400

    remove basename + add homepage

--- frontend/src/components/Routes/routes.tsx
+++ frontend/src/components/Routes/routes.tsx
@@ -1,18 +1,17 @@
 import { Pages } from "../pages";
 import { createRoutes } from "./routes-config";
 
-const basename = "culinarist.github.io/lt-event-app/";
 export const routes = createRoutes({
   fallback_route: "/404",
   layouts: {},
   routes: [
     { path: "*", element: <Pages.Error /> },
     {
-      path: `${basename}/bingo`,
+      path: "/bingo",
       element: <Pages.Bingo />,
     },
     {
-      path: `${basename}/`,
+      path: "/",
       element: <Pages.Home />,
     },
   ],
