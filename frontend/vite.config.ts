9b2e29a8044e285b7d00be1dd6feee0d7a7d7457
Alex <alex.e.cash28@gmail.com>
2023-06-13 00:46:58 -0400

    updated dist locations

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "dist",
+    outDir: "../dist",
   },
+  base: "./",
   plugins: [react()],
   server: {
     port: 3000,
