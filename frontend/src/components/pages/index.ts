d71f7b919d1be589eb9a8fe12e69297831c1efc6
Alex <alex.e.cash28@gmail.com>
2023-06-21 14:07:30 -0400

    add icon + fix export name

--- frontend/src/components/pages/index.ts
+++ frontend/src/components/pages/index.ts
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 
 export const Pages = {
-  Bingo: lazy(() => import("./TeamData")),
+  TeamData: lazy(() => import("./TeamData")),
   Home: lazy(() => import("./Home")),
   Error: lazy(() => import("./Error")),
 };
