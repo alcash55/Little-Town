9f8199cc61713bfdc5c0ab463988ef906491a261
Sami <sami.niemi@live.fi>
2023-06-23 02:08:49 +0300

    Remove example code

--- backend/app/routers/events.py
+++ backend/app/routers/events.py
@@ -14,11 +14,3 @@ def create_event(event: schemas.EventCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_event)
     return db_event
-
-
-# @app.post("/users/", response_model=schemas.User)
-# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_by_email(db, email=user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     return crud.create_user(db=db, user=user)
