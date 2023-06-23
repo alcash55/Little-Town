77b7c0bf27b32181a3f996fd1331b77b213de369
Alex <alex.e.cash28@gmail.com>
2023-06-23 00:48:32 -0400

    first attempt to use googlesheets api

--- frontend/src/components/pages/BingoBoard/BingoBoard.tsx
+++ frontend/src/components/pages/BingoBoard/BingoBoard.tsx
@@ -1,6 +1,17 @@
 import { Box, Typography } from "@mui/material";
+import axios from "axios";
 
 const BingoBoard = () => {
+  const getData = async () => {
+    try {
+      const { data: data } = await axios.get("/api/data");
+      return data;
+    } catch (e) {
+      console.log(e);
+      return "unable to get data";
+    }
+  };
+
   return (
     <Box>
       <Typography>Bingo Board</Typography>
