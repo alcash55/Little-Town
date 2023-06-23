17db78d67f20bd12d9d2a1e3b3677d1b197196a7
Alex <alex.e.cash28@gmail.com>
2023-06-23 02:09:15 -0400

    add mock bingo board

--- frontend/src/components/pages/index.ts
+++ frontend/src/components/pages/index.ts
@@ -1,9 +1,9 @@
-import { lazy } from "react";
+import { lazy } from 'react';
 
 export const Pages = {
-  TeamData: lazy(() => import("./TeamData")),
-  Home: lazy(() => import("./Home")),
-  Error: lazy(() => import("./Error")),
-  BingoBoard: lazy(() => import("./BingoBoard")),
-  TeamProgress: lazy(() => import("./TeamProgress")),
+  TeamData: lazy(() => import('./TeamData')),
+  Home: lazy(() => import('./Home')),
+  Error: lazy(() => import('./Error')),
+  BingoBoard: lazy(() => import('./BingoBoard')),
+  BingoData: lazy(() => import('./BingoData')),
 };
