ae3000a96d7c2caf5c29283edc17f13d2302a103
Sami <sami.niemi@live.fi>
2023-07-07 20:08:02 +0300

    get whole sheet in the cache

--- backend/app/routers/google_sheets.py
+++ backend/app/routers/google_sheets.py
@@ -10,22 +10,24 @@ router = APIRouter()
 
 
 @timed_lru_cache(seconds=10)
-def read_sheet_data(sheet_id, range):
+def read_sheet_data(sheet_id, tab_name):
     print("Fetching data from sheet instead of cache")
-    sheet = google_sheets_service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=sheet_id, range=range).execute()
-    values = result.get("values", [])
-    return values
+    request = (
+        google_sheets_service.spreadsheets()
+        .values()
+        .get(spreadsheetId=sheet_id, range=tab_name, majorDimension="ROWS")
+    )
+    response = request.execute()
+    return response["values"]
 
 
 @router.get("/sheets/{sheet_id}/tab/{tab_name}/range/{range_id}")
 async def get_range(
     sheet_id: Annotated[str, Path()],
     tab_name: Annotated[str, Path()],
-    range_id: Annotated[str, Path()]
+    range_id: Annotated[str, Path()],
 ):
-    range = f"{tab_name}!{range_id}"
-    values = read_sheet_data(sheet_id, range)
+    values = read_sheet_data(sheet_id, tab_name=tab_name)
     return values
 
 
@@ -34,9 +36,9 @@ async def get_cell(
     # Example function to retrieve data from a Google Sheet
     sheet_id: Annotated[str, Path()],
     tab_name: Annotated[str, Path()],
-    cell_id: Annotated[str, Path()]
+    cell_id: Annotated[str, Path()],
 ):
-    range =f"{tab_name}!{cell_id}"
+    range = f"{tab_name}!{cell_id}"
     # Make a request to the Google Sheets API
     sheet = google_sheets_service.spreadsheets()
     result = sheet.values().get(spreadsheetId=sheet_id, range=range).execute()
@@ -49,9 +51,9 @@ async def insert_to_cell(
     cell_item: schemas.CellItem,
     sheet_id: Annotated[str, Path()],
     tab_name: Annotated[str, Path()],
-    cell_id: Annotated[str, Path()]
+    cell_id: Annotated[str, Path()],
 ):
-    range =f"{tab_name}!{cell_id}"
+    range = f"{tab_name}!{cell_id}"
     # TODO: insert cell item to cell
     sheet = google_sheets_service.spreadsheets()
     result = sheet.values().get(spreadsheetId=sheet_id, range=range).execute()
