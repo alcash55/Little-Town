7e40b07105ec72ecc97f037d296313d5047a8979
Alex <alex.e.cash28@gmail.com>
2023-09-01 00:00:32 -0400

    added loading container

---
+++ frontend/src/components/LoadingContainer/LoadingContainer.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react';
+import { Box } from '@mui/material';
+import LittleTownAnimation from '../../assets/Images/LittleTownAnimation.gif';
+import { darkTheme } from '../../layout/Theme';
+import React from 'react';
+
+interface Props {
+  loading: boolean;
+  children: ReactNode;
+  [key: string]: any;
+  width: number;
+  height: number;
+}
+
+export const LoadingContainer = ({ width, height, loading, children, ...rest }: Props) => {
+  if (loading) {
+    return (
+      <Box
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'100vh'}
+        overflow={'hidden'}
+        p={0}
+        sx={{ bgcolor: darkTheme.palette.secondary.main }}
+      >
+        <img
+          src={LittleTownAnimation}
+          alt="Loading..."
+          height={320}
+          width={189}
+          style={{ borderRadius: '50%', objectFit: 'cover', width: width, height: height }}
+        />
+      </Box>
+    );
+  }
+
+  return <React.Fragment {...rest}>{children}</React.Fragment>;
+};
