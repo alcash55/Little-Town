7f81470023df3fb6be0edb79edce91647759d1cd
Alex <alex.e.cash28@gmail.com>
2023-09-01 00:23:42 -0400

    update build

---
+++ dist/assets/index-f4fb5743.js
@@ -0,0 +1,1 @@
+import{g as r,B as t}from"./index-4c2ad99e.js";import{T as o}from"./Shell-271381c2.js";const e=()=>r(t,{sx:{bgcolor:"#424242",height:"100%",display:"flex",justifyContent:"center",alignItems:"center"},children:r(o,{variant:"h1",children:"404"})}),a=e;export{a as default};
