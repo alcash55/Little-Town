5747ac221f52dece92cc3e4b239b336e1acc878b
Alex <alex.e.cash28@gmail.com>
2023-09-06 00:28:06 -0400

    setup functions framework

--- nodeBackend/src/index.ts
+++ nodeBackend/src/index.ts
@@ -1,1 +1,15 @@
-import fs from "fs";
+import { HttpFunction } from "@google-cloud/functions-framework";
+
+export const LittleTownFunctions: HttpFunction = async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "OPTIONS") {
+    // Send response to OPTIONS requests
+    res.set("Access-Control-Allow-Methods", "GET");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "3600");
+    res.status(204).send("");
+  }
+
+  res.send("hello");
+};
