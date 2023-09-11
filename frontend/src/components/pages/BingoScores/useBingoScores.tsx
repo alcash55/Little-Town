ed08435c0eb7f2aa3e9cf9939bc63013888e3049
Alex <alex.e.cash28@gmail.com>
2023-09-11 04:32:32 -0400

    remove comments + mock code

--- frontend/src/components/pages/BingoScores/useBingoScores.tsx
+++ frontend/src/components/pages/BingoScores/useBingoScores.tsx
@@ -1,21 +1,3 @@
 import { useState, useEffect } from 'react';
 
-export const useBingoScores = async () => {
-  let headersList = {
-    Accept: '*/*',
-    'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
-  };
-
-  let response = await fetch(
-    'http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=lucky%20buck2',
-    {
-      method: 'GET',
-      headers: headersList,
-    },
-  );
-
-  let data = await response.json();
-  console.log(data);
-
-  return data;
-};
+export const useBingoScores = async () => {};
