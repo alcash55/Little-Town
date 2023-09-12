58638b6a276d1dbd93c9ebc7cf97b4ead3a369ee
Alex <alex.e.cash28@gmail.com>
2023-09-12 03:00:59 -0400

    working json output

--- frontend/src/components/pages/BoardGame/BoardGame.tsx
+++ frontend/src/components/pages/BoardGame/BoardGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Box,
   Stack,
@@ -12,16 +12,27 @@ import {
 } from '@mui/material';
 import { darkTheme } from '../../../layout/Theme';
 import { bossList } from './bossList';
+import { Tile } from '../../types';
 
 const BoardGame = () => {
   const [type, setType] = useState<string>('A');
   const [task, setTask] = useState('');
   const [instructions, setInstructions] = useState('');
   const [selectedBoss, setSelectedBoss] = useState('');
+  const [tileName, setTileName] = useState('');
   const [iconUrl, setIconUrl] = useState('');
+  const [json, setJson] = useState<Tile>({
+    name: tileName,
+    tile_type: type,
+    task: task,
+    instructions: instructions,
+    icon_url: iconUrl,
+  });
+
   const bosses = bossList();
 
   const textfieldStyles = {
+    width: '50%',
     '& .MuiOutlinedInput-root': {
       '& fieldset': {
         borderColor: 'white', // White border color
@@ -42,6 +53,8 @@ const BoardGame = () => {
   };
 
   const selectStyles = {
+    width: '50%',
+
     '& .MuiInputBase-input': {
       color: 'white', // White text color
     },
@@ -63,8 +76,9 @@ const BoardGame = () => {
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
-        gap: 2,
+        gap: 3,
         mt: 2,
+        width: '100%',
       }}
     >
       <TextField
@@ -101,13 +115,15 @@ const BoardGame = () => {
             color: 'white',
           }}
         >
-          Select Tile Type
+          Select Boss
         </InputLabel>
         <Select
           id="iconUrl"
           label="Select Boss"
           value={selectedBoss}
-          onChange={(e) => setSelectedBoss(e.target.value)}
+          onChange={(e) => {
+            setSelectedBoss(e.target.value);
+          }}
           sx={selectStyles}
         >
           {bosses.map((boss) => (
@@ -120,108 +136,137 @@ const BoardGame = () => {
     </Box>
   );
 
-  const typeB = () => (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        gap: 2,
-        mt: 2,
-      }}
-    >
-      <TextField
-        fullWidth
-        variant="outlined"
-        multiline
-        id="task"
-        label="Task"
-        sx={textfieldStyles}
-        InputLabelProps={{
-          style: { color: 'white' },
+  const typeB = () => {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          gap: 3,
+          mt: 2,
+          width: '100%',
         }}
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <TextField
-        fullWidth
-        variant="outlined"
-        multiline
-        id="instructions"
-        label="Number of Tiles"
-        sx={textfieldStyles}
-        InputLabelProps={{
-          style: { color: 'white' },
-        }}
-        value={instructions}
-        onChange={(e) => setInstructions(e.target.value)}
-      />
-      {/* <TextField
-        variant="outlined"
-        multiline
-        id="icon"
-        label="Go Back Icon"
-        inputProps={{ color: 'white' }}
-        sx={textfieldStyles}
-        value={
-          'https://static.vecteezy.com/system/resources/thumbnails/000/589/654/small/40_436.jpg'
-        }
-        onChange={(e) => setInstructions(e.target.value)}
-      /> */}
-    </Box>
-  );
+      >
+        <TextField
+          fullWidth
+          variant="outlined"
+          multiline
+          id="task"
+          label="Task"
+          sx={textfieldStyles}
+          InputLabelProps={{
+            style: { color: 'white' },
+          }}
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <TextField
+          fullWidth
+          variant="outlined"
+          multiline
+          id="instructions"
+          label="Number of Tiles"
+          sx={textfieldStyles}
+          InputLabelProps={{
+            style: { color: 'white' },
+          }}
+          value={instructions}
+          onChange={(e) => setInstructions(e.target.value)}
+        />
+      </Box>
+    );
+  };
 
-  const createJSON = () => {
-    console.log('clicked');
+  const handleSelectedIcon = (name: string) => {
+    setSelectedBoss(name);
+    if (type === 'A') {
+      const formatedName = name.replace(/\s+/g, '_');
+      const baseIconUrl = `https://oldschool.runescape.wiki/images/thumb/${formatedName}.png/84px-${formatedName}.png?33092`;
+      setIconUrl(baseIconUrl);
+    } else {
+      setIconUrl(
+        'https://static.vecteezy.com/system/resources/thumbnails/000/589/654/small/40_436.jpg',
+      );
+    }
   };
 
   const handleTypeSelect = (value: string) => {
     setType(value);
   };
 
+  useEffect(() => {
+    handleSelectedIcon(selectedBoss);
+    setInstructions(instructions);
+    setTask(task);
+    setType(type);
+    setTileName(tileName);
+    setJson({
+      name: tileName,
+      tile_type: type,
+      task: task,
+      instructions: instructions,
+      icon_url: iconUrl,
+    });
+  }, [task, type, tileName, selectedBoss, instructions]);
+
   return (
-    <Box height={'100%'} width={'100%'} p={3} sx={{ bgcolor: darkTheme.palette.primary.main }}>
-      <Stack spacing={2} justifyContent="center" alignItems="center" textAlign={'center'}>
-        <Box width={'50%'}>
-          <Typography variant="h1" paddingBottom={10}>
-            Board Game Config
-          </Typography>
+    <Stack
+      spacing={3}
+      justifyContent="center"
+      alignItems="center"
+      textAlign={'center'}
+      width={'100%'}
+      height={'100%'}
+      p={3}
+      sx={{ bgcolor: darkTheme.palette.primary.main }}
+    >
+      <Typography variant="h1" paddingBottom={10} fontSize={64}>
+        Board Game Config
+      </Typography>
 
-          <FormControl fullWidth>
-            <InputLabel
-              id="instructions"
-              sx={{
-                color: 'white',
-              }}
-            >
-              Select Tile Type
-            </InputLabel>
-            <Select
-              variant="outlined"
-              id="tileType"
-              label="Select Tile Type"
-              value={type}
-              onChange={(e) => handleTypeSelect(e.target.value as string)}
-              sx={selectStyles}
-            >
-              <MenuItem value={'A'}>A - Get Collection Log Drop</MenuItem>
-              <MenuItem value={'B'}>B - Go Back X amount of Tiles</MenuItem>
-            </Select>
-          </FormControl>
-
-          {type === 'A' ? typeA() : type === 'B' ? typeB() : <></>}
-        </Box>
-        <Button
-          onClick={createJSON}
-          variant="outlined"
-          size="large"
-          sx={{ color: 'white', borderColor: 'white' }}
+      <FormControl fullWidth>
+        <InputLabel
+          id="instructions"
+          sx={{
+            color: 'white',
+          }}
         >
-          Submit
-        </Button>
-      </Stack>
-    </Box>
+          Select Tile Type
+        </InputLabel>
+        <Select
+          variant="outlined"
+          id="tileType"
+          label="Select Tile Type"
+          value={type}
+          onChange={(e) => handleTypeSelect(e.target.value as string)}
+          sx={selectStyles}
+        >
+          <MenuItem value={'A'}>A - Get Collection Log Drop</MenuItem>
+          <MenuItem value={'B'}>B - Go Back X amount of Tiles</MenuItem>
+        </Select>
+      </FormControl>
+
+      <TextField
+        fullWidth
+        variant="outlined"
+        id="icon"
+        label="Tile Name"
+        sx={textfieldStyles}
+        value={tileName}
+        onChange={(e) => setTileName(e.target.value)}
+        InputLabelProps={{
+          style: { color: 'white' },
+        }}
+      />
+
+      {type === 'A' ? typeA() : type === 'B' ? typeB() : <></>}
+
+      <Typography width={'100%'} noWrap={false}>
+        {JSON.stringify(json)}
+      </Typography>
+    </Stack>
   );
 };
 
