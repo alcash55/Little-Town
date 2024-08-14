5460955e801a8c88703d292c6b42c88ae3de40a7
Alex <alex.e.cash28@gmail.com>
2024-08-14 14:01:42 -0400

    remvoe unused packages, unused files, update error page

--- frontend/src/components/pages/Error/Error.tsx
+++ frontend/src/components/pages/Error/Error.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 
 const Error = () => {
   return (
@@ -7,11 +7,16 @@ const Error = () => {
         bgcolor: '#424242',
         height: '100%',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        gap: 5,
       }}
     >
-      <Typography variant="h1">404</Typography>
+      <Typography variant="h1">404 - Page Not Found</Typography>
+      <Button size="large" variant="contained" href="/">
+        Go Home
+      </Button>
     </Box>
   );
 };
