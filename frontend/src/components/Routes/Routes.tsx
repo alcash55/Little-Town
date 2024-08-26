c994bb1456d46fbbdebe5a9747327959cb2a4d21
Alex <alex.e.cash28@gmail.com>
2024-08-26 01:13:19 -0400

    remove deploy dev

--- frontend/src/components/Routes/Routes.tsx
+++ frontend/src/components/Routes/Routes.tsx
@@ -13,12 +13,12 @@ export const Routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Providers />}>
       <Route index element={<Pages.Home />} />
-      <Route path="/TeamData" element={<Pages.TeamData />} />
-      <Route path="/BingoBoard" element={<Pages.BingoBoard />} />
-      <Route path="/BingoScores" element={<Pages.BingoScores />} />
-      <Route path="/BingoRules" element={<Pages.BingoRules />} />
-      <Route path="/AdminPanel" element={<Pages.AdminPanel />} />
-      <Route path="/*" element={<Pages.Error />} />
+      <Route path="TeamData" element={<Pages.TeamData />} />
+      <Route path="BingoBoard" element={<Pages.BingoBoard />} />
+      <Route path="BingoScores" element={<Pages.BingoScores />} />
+      <Route path="BingoRules" element={<Pages.BingoRules />} />
+      <Route path="AdminPanel" element={<Pages.AdminPanel />} />
+      <Route path="*" element={<Pages.Error />} />
     </Route>,
   ),
 );
