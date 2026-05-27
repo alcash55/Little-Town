026016db676ac127b412888b1f9e368e2add20c2
Alex <alex.e.cash28@gmail.com>
2026-05-26 22:30:15 -0400

    chore: update dependencies in yarn.lock and enhance PageLayout usage
    - Upgraded various dependencies in yarn.lock for improved performance and security.
    - Modified UserInvite and Resources components to utilize the maxWidth prop in PageLayout for better layout control.
    - Updated PageLayout to allow optional children prop, enhancing flexibility in component usage.

--- frontend/src/components/Pages/AdminPanel/UserInvite/UserInvite.tsx
+++ frontend/src/components/Pages/AdminPanel/UserInvite/UserInvite.tsx
@@ -1,7 +1,9 @@
 import PageLayout from '../../../../layout/PageLayout/PageLayout';
 
 const UserInvite = () => {
-  return <PageLayout title="User Invite" />;
+    return <PageLayout title="User Invite" maxWidth="full">
+        <></>
+    </PageLayout>;
 };
 
 export default UserInvite;
--- frontend/src/components/Pages/Resources/Resources.tsx
+++ frontend/src/components/Pages/Resources/Resources.tsx
@@ -4,7 +4,7 @@ import PageLayout from '../../../layout/PageLayout/PageLayout';
  * Resource page that shows tips and info for OSRS bossing and others
  */
 const Resources = () => {
-  return <PageLayout title="Resources" />;
+  return <PageLayout title="Resources" maxWidth="full" />;
 };
 
 export default Resources;
